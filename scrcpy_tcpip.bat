988503a803ab682a69e49c0304a189f784da3c57
AlejandroGamboaR <103536288+AlejandroGamboaR@users.noreply.github.com>
2022-04-22 07:44:50 -0600

    subtest3 change
    subtest3 change

--- scrcpy_tcpip.bat
+++ scrcpy_tcpip.bat
@@ -1,4 +1,4 @@
 @echo off
-scrcpy.exe --tcpip=192.168.0.5 --turn-screen-off --stay-awake --power-off-on-close
+scrcpy.exe --tcpip=192.168.0.5 --turn-screen-off --stay-awake --power-off-on-close --max-size 800
 :: if the exit code is >= 1, then pause
 if errorlevel 1 pause
